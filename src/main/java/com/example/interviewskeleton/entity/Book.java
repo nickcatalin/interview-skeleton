8b76543ec50705509812cd299ea0085c9fd4df7f
catalin <catalin.nicola@esolutions.ro>
2023-11-14 19:02:18 +0200

    Initial commit

---
+++ src/main/java/com/example/interviewskeleton/entity/Book.java
@@ -0,0 +1,56 @@
+package com.example.interviewskeleton.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private int year;
+    private String seoName;
+    private String productCode;
+
+    public void generateSeoName() {
+        this.seoName = author + " - " + title + " (" + year + ")";
+    }
+
+    public void generateProductCode() {
+        if (seoName == null) {
+            productCode = null;
+            return;
+        }
+
+        StringBuilder codeBuilder = new StringBuilder();
+        for (char ch : seoName.toCharArray()) {
+            if (ch == '(' || ch == ')' || ch == '-' || ch == ' ') {
+                codeBuilder.append('.');
+            } else if ("AEIOUaeiou".contains(String.valueOf(ch))) {
+                codeBuilder.append('1');
+            } else if (Character.isAlphabetic(ch)) {
+                codeBuilder.append('0');
+            } else if (isNumber(ch)) {
+                codeBuilder.append(ch);
+            }
+        }
+        this.productCode = codeBuilder.toString();
+    }
+
+    boolean isNumber(final char ch) {
+        return ch >= '0' && ch <= '9';
+    }
+}
---
+++ src/main/java/com/example/interviewskeleton/service/BookService.java
@@ -0,0 +1,19 @@
+package com.example.interviewskeleton.service;
+
+import com.example.interviewskeleton.entity.Book;
+import com.example.interviewskeleton.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public Book addBook(Book book) {
+        book.generateSeoName();
+        book.generateProductCode();
+
+        return bookRepository.save(book);
+    }
+}
